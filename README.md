98090c00a1d0bc149fa101e7a5eb312da272524d
Carla Edila Silveira <rosa.carla@pucpr.edu.br>
2022-04-04 01:14:45 -0300

    atualizado link do projeto

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 ## Installation
 
-Use the package manager [pip](https://pip.pypa.io/en/stable/) to install package_simple_dio
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install package_simple_dio.
 
 ```bash
 pip install package_simple_dio
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     description="Example of package creation",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/rosacarla/package_simple_project",
+    url="https://github.com/rosacarla/Package_simple_project",
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
